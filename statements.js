85607eabca622d70ff26318bac889edca4f823ce
tobias <tobias.omondi@student.moringaschool.com>
2023-10-29 13:07:40 +0300

    updated

---
+++ statements.js
@@ -0,0 +1,11 @@
+// Node.js is important for web development in three ways:
+
+// you can use it to write server-side software in JavaScript.
+// • You can also use it to write software for the command line (think Unix shell, Win-
+// dows PowerShell, etc.). Many JavaScript-related tools are based on (and executed
+// via) Node.js.
+// • Node’s software registry, npm, has become the dominant way of installing tools
+// (such as compilers and build tools) and libraries – even for client-side develop-
+// ment.
+
+console.log("Hello, Toby!")
